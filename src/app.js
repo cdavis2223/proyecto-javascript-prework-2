616b1d10c0c9c6025f7f77a49e1d70749b2b6a66
cdavis2223 <cdavis2223@gmail.com>
2023-11-06 23:51:29 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,38 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
+  document.querySelector("#generar").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Hola Mundo");
+};
+
+let generateExcuse = () => {
+  let quien = ["Mi perro", "Mi abuela", "Mi jefa", "Mi hermana"];
+  let accion = ["comió", "orinó", "quebró", "quemó", "robó"];
+  let que = ["mi libro", "mis llaves", "mi carro", "mi computadora"];
+  let cuando = [
+    "antes de clase",
+    "justo ahora",
+    "cuando terminé",
+    "durante el almuerzo",
+    "mientras dormía"
+  ];
+
+  let arrayQuien = Math.floor(Math.random() * quien.length);
+  let arrayAccion = Math.floor(Math.random() * accion.length);
+  let arrayQue = Math.floor(Math.random() * que.length);
+  let arrayCuando = Math.floor(Math.random() * cuando.length);
+
+  return (
+    quien[arrayQuien] +
+    " " +
+    accion[arrayAccion] +
+    " " +
+    que[arrayQue] +
+    " " +
+    cuando[arrayCuando]
+  );
 };
